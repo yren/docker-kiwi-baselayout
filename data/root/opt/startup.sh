8abb080718846671c779161ecb353d63db53cad3
Yufei Ren <yufei.ren@thomsonreuters.com>
2017-06-28 23:34:21 +0800

    minor change

--- data/root/opt/startup.sh
+++ data/root/opt/startup.sh
@@ -2,17 +2,13 @@
 set -e
 
 # USE the trap if you need to also do manual cleanup after the service is stopped,
-#     or need to start multiple services in the one container
+# or need to start multiple services in the one container
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
 # Put your own startup command here
 echo "This is kiwi/baselayout"
 echo I am
 whoami
-echo
-export
-
-/bin/bash
 
 # stop service and clean up here
 echo "stopping service..."
